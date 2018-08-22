147244a85214992191318bd8fbdc763a20ebfddd
Unknown <tohrul@ukr.net>
2018-08-22 16:05:37 +0300

    wp: разделены файлы.

--- muslimwomen-pro/footer.php
+++ muslimwomen-pro/footer.php
@@ -5,3 +5,55 @@
  * Date: 22.08.2018
  * Time: 15:54
  */
+
+?>
+</main>
+<!-- конец основного контента -->
+
+<!-- подвал -->
+<footer class="footer">
+    <div class="container">
+        <span>Всегда с ВАМи!</span>
+        <div class="social-footer d-flex flex-row justify-content-center align-items-center">
+            <a class="mw-social--link" href="#">
+                <i class="fab fa-facebook-f fa-2x"></i>
+            </a>
+            <a class="mw-social--link" href="#">
+                <i class="fab fa-instagram fa-2x"></i>
+            </a>
+            <a class="mw-social--link" href="#">
+                <i class="fab fa-youtube fa-2x"></i>
+            </a>
+        </div>
+    </div>
+</footer>
+<!-- конец подвала -->
+</div>
+</div>
+</div>
+
+<!-- объявление -->
+<div id="feedback">
+    <a href="#popup1">Объявление!</a>
+</div>
+
+<div id="popup1" class="overlay">
+    <div class="popup">
+        <div class="div1">
+            <a class="close" href="#">
+                <i class="fas fa-times fa-2x"></i>
+            </a>
+            <img class="popup-image" src="images/author.jpg" alt="Adverts">
+        </div>
+    </div>
+</div>
+<!-- конец объявления -->
+
+<script type="application/javascript" src="js/jquery-3.3.1.js"></script>
+<script type="application/javascript" src="js/bootstrap.js"></script>
+<script type="application/javascript" src="js/menu.js"></script>
+<script type="application/javascript" src="js/loader.js"></script>
+
+<?php wp_footer(); ?>
+</body>
+</html>
